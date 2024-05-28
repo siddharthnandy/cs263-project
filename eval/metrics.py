f6466903d88cdb6985e11d93dd065076f6ddf621
Siddharth Nandy <sidd.nandy@gmail.com>
2024-05-28 15:21:44 -0700

    qwk metric

--- eval/metrics.py
+++ eval/metrics.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from scipy.stats import spearmanr, kendalltau
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, cohen_kappa_score
 
 
 def get_stats(rater1_scores : pd.Series , rater2_scores : pd.Series):
@@ -12,6 +12,8 @@ def get_stats(rater1_scores : pd.Series , rater2_scores : pd.Series):
     spearman_corr = spearmanr(rater1_scores, rater2_scores)
     kendall_tau = kendalltau(rater1_scores, rater2_scores)
     mse = mean_squared_error(rater1_scores, rater2_scores)
+    qwk = cohen_kappa_score(rater1_scores, rater2_scores, weights='quadratic')
+
     
     return {
         'spearman_corr_stat': spearman_corr.statistic,
@@ -19,4 +21,5 @@ def get_stats(rater1_scores : pd.Series , rater2_scores : pd.Series):
         'kendall_tau_stat': kendall_tau.statistic,
         'kendall_tau_pval': kendall_tau.pvalue,
         'mean_square_error': mse,
+        'quadratic_weighted_kappa': qwk
     }
